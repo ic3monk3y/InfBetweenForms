6462247711c15c15ba82d2fc4dd852b5e0b53f77
Hector Octavio Tirado Navarro <hector.t.navarro.99@gmail.com>
2023-11-27 11:57:03 -0600

    Add project files.

---
+++ WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormsCommunication
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private Form1 mainForm = null;
+        public Form2(Form callingForm)
+        {
+            mainForm = callingForm as Form1; 
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.mainForm.LabelText = txtMessage.Text;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FormsCommunication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
